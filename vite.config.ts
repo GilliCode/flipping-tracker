aa5ee24aaf10c44997e3ad9fb8ac1e23c125f042
GilliCode <116256736+GilliCode@users.noreply.github.com>
2025-03-05 01:53:39 +0800

    Temporary commit before rolling back

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base = process.env.NODE_ENV === 'production' ? '/flipping-tracker/' : '/'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/flipping-tracker/' // Fix asset paths for GitHub Pages
+  base
 })
